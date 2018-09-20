42ea638e7a8f687a3eb1c259520916f48ea00ae8
Mark Schauer <schau303@neoprene.morris.umn.edu>
2018-09-20 11:14:20 -0500

    Add code to `disemvowel.c`, partially working but needs fixing

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -2,6 +2,37 @@
 
 #include "disemvowel.h"
 
+int check_vowel(char);
+
+int check_vowel(char c)
+{
+  switch(c) {
+    case 'a':
+    case 'A':
+    case 'e':
+    case 'E':
+    case 'i':
+    case 'I':
+    case 'o':
+    case 'O':
+    case 'u':
+    case 'U':
+      return 1;
+    default:
+      return 0;
+  }
+}
+
+int i, j = 0;
+char t[100];
+
 char *disemvowel(char *str) {
-  return (char*) "";
+
+	for(i = 0; str[i] != '\0'; i++) {
+		if(check_vowel(str[i]) == 0) {
+			t[j] = str[i];
+			j++;
+		}
+	} t[j] = '\0';	
+  return  t;
 }
